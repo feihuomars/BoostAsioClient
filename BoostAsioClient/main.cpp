875d00cd2d3949238fc2afb3cc7135714d326d7d
feihuomars <feihuomars@outlook.com>
2018-04-28 19:32:56 +0800

    更改main函数，增加代理操作

--- BoostAsioClient/main.cpp
+++ BoostAsioClient/main.cpp
@@ -12,7 +12,7 @@ void start(string ip, string path, string startTIme, string endTime, string pict
 
 	boost::asio::io_service ioService;
 	boost::asio::ip::tcp::resolver resolver(ioService);
-	auto endpointIterator = resolver.resolve({ ip, "1000" });
+	auto endpointIterator = resolver.resolve({ "ngrok.cloudserver.xin", ip });
 	Client client(ioService, endpointIterator, path, startTIme, endTime, pictureID, errorCode, zipCode);
 	ioService.run();
 
@@ -34,7 +34,15 @@ int main(int argc, char* argv[])
 {
 	
 	vector<string> ipArr;
-	ipArr.push_back("127.0.0.1");
+	//ipArr.push_back("8082");
+	ipArr.push_back("9090");
+	ipArr.push_back("9094");
+	ipArr.push_back("9092");
+	ipArr.push_back("9091");
+	//ipArr.push_back("127.0.0.1");
+	//ipArr.push_back("10.59.13.178");
+	//ipArr.push_back("10.59.13.212");
+	//ipArr.push_back("10.59.13.193");
 	//ipArr.push_back("10.59.13.222");
 	//ipArr.push_back("10.59.13.219");
 	//ipArr.push_back("1000");
@@ -49,7 +57,7 @@ int main(int argc, char* argv[])
 	}*/
 	//参数：ip向量，图片路径，开始时间，截至时间，图片id
 	//MultiClient multiClient(ipArr, "D:/test/test.txt", "2017-11-11 11:11:11", "2017-12-12 12:12:12", "233");
-	startSingleClient(ipArr, "D:/test/12.jpg", "2017-11-11 11:11:11", "2017-12-12 12:12:12", "233", "0", "5768444");
+	startSingleClient(ipArr, "D:/test/xiaohan.jpg", "2017-11-11 11:11:11", "2017-12-12 12:12:12", "233", "0", "5768444");
 	system("pause");
 
 	return 0;
